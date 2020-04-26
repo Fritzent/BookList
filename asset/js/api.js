dc49b07c36eeb80232b2ec0512a22aa39c53cfd1
Fritzent <afritzent@gmail.com>
2020-04-26 20:22:23 +0700

    Fix Show Data

--- asset/js/api.js
+++
@@ -1,5 +0,0 @@
-const apiKey = 'fritzent';
-const topHeadlinesUrl = 
-'https://fullstack-book.ariefdfaltah.com/book/list?key='+apiKey;
-
-export { apiKey , topHeadlinesUrl } ;
---
+++ asset/js/datashow.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $.ajax({
+        url : "https://fullstack-book.ariefdfaltah.com/book/list?key=fritzent",
+        success: function(data) {
+            for(var i = 0 ; i < data.meta.data_count ; i++){
+                console.log(data.data[i]);
+                var book = 
+                '<tr>' + 
+                '<td>' + data.data[i].title +'</td>' +
+                '<td>' + data.data[i].category +'</td>' +
+                '<td>' + data.data[i].description +'</td>' +
+                '<td class="toolUpdate"> <button class="btn-update"> update </button> </td>' +
+                '<td class="toolDelete"> <button class="btn-delete"> delete </button> </td>' +
+                '</tr>';
+
+                $('.list-book').append(book);
+            }
+        }
+    });
+})
+
+eh 
